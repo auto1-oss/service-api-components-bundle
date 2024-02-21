96d7f519f920261428913185f7a3ff59127e3e6a
Ivan Pidhorodetskyi <ivan.pidhorodetskyi@auto1.com>
2024-02-21 17:01:37 +0100

    Symfony 6 support
    * update tests to support php versions >=7.1

--- Tests/Service/Endpoint/EndpointsConfigurationLoaderTest.php
+++ Tests/Service/Endpoint/EndpointsConfigurationLoaderTest.php
@@ -4,15 +4,12 @@ namespace Auto1\ServiceAPIComponentsBundle\Tests\Service\Endpoint;
 
 use PHPUnit\Framework\TestCase;
 use Auto1\ServiceAPIComponentsBundle\Service\Endpoint\EndpointsConfigurationLoader;
-use Prophecy\PhpUnit\ProphecyTrait;
 
 /**
  * Class EndpointsConfigurationLoaderTest
  */
 class EndpointsConfigurationLoaderTest extends TestCase
 {
-    use ProphecyTrait;
-
     public function testConstructor(): void
     {
         $endpointsConfigurationLoader = new EndpointsConfigurationLoader('configFilePathString');
